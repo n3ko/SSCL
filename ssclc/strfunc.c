8d7e6d1d75a5e9b86f89df7cd87235f1bb8a7b76
Szilard Hajba <szilu@symbion.hu>
2006-11-08 22:36:39 +0000

    str_cmp_locale() substring bugfix

--- ssclc/strfunc.c
+++ ssclc/strfunc.c
@@ -292,5 +292,5 @@ int str_cmp_locale(Locale *loc, const char *s1, const char *s2)
 	v2=loc->def[c2-loc->bdef] ? loc->def[c2-loc->bdef] : c2<<loc->bitshift;
 //	fprintf(stderr, "str_locale_cmp: %08x ?= %08x (%08x ?= %08x)\n", c1, c2, v1, v2);
     }
-    return v1-v2;
+    return *s1 && *s2 ? v1-v2 : *s1-*s2;
 }
